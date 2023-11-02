fa4c5ed80d537a97c7c2f0c9119b234b94f42ab8
Alex Isaacs <alexisaacs18@gmail.com>
2023-11-02 16:15:48 -0400

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 const form = document.getElementById('create-task-form')
 
-const tasks = document.getElementById('tasks')
+const li = document.getElementById('tasks')
 // document.addEventListener("DOMContentLoaded", () => {
 // your code here
 // });
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
+  // console.log(form.desc.value)
   addTask(form.desc.value)
 })
 
-function addTask(tasksString) {
-  task = document.createElement('li')
-  head = document.createElement('h1')
-  tasks.appendChild = tasks
-  tasks.appendChild = head
-  tasksString
+function addTask(taskString) {
+  let task = document.createElement('li')
+  task.textContent = taskString
+  li.append(task)
 }
