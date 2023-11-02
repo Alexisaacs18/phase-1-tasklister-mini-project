c24ee3e94b96b4d83ad2fe20358106b93278b393
Alex Isaacs <alexisaacs18@gmail.com>
2023-11-02 15:56:44 -0400

    Mid way

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const form = document.getElementById('create-task-form')
+
+const tasks = document.getElementById('tasks')
+// document.addEventListener("DOMContentLoaded", () => {
+// your code here
+// });
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  addTask(form.desc.value)
+})
+
+function addTask(tasksString) {
+  task = document.createElement('li')
+  head = document.createElement('h1')
+  tasks.appendChild = tasks
+  tasks.appendChild = head
+  tasksString
+}
